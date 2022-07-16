420ebeeb9e42b66e64369a720549946fbad60f77
Luiz Henrique <luiz.paz@bandtec.com.br>
2022-07-16 01:54:22 -0300

    Fix(v 4.0): Sincronizando funcionamento

--- v 4.0 - Em desenvolvimento/Site Institucional/API-projeto-python/credentials.py
+++
@@ -1,9 +0,0 @@
-usr_L = 'root'
-pswd_L = 'Urubu100'
-banco_L = 'banco_overwatch'
-
-server_N = 'overwatch.database.windows.net'
-driver_N = 'SQL Server'
-usr_N = 'overwatch'
-pswd_N = 'Azure123#'
-data_N = 'OverwatchServer'
